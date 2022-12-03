12f17c530a90f9eb21a8be6a71801e66d8bba8c5
EmmanuelBalderasB <ebalderasbeq@gmail.com>
2022-12-03 01:04:12 -0600

    removes background image changes

--- scripts/app.js
+++ scripts/app.js
@@ -1,54 +1,56 @@
 const getInput = document.getElementsByTagName('input');
-const body = document.querySelector('body');
+/* const body = document.querySelector('body');
 
 const colors = '../resources/images/colors.jpg';
 const wall = '../resources/images/wall.jpg';
-const white = '../resources/images/white.jpg'
-const medium = '../resources/images/medium.jpg';
+const white = '../resources/images/white.jpg';
+const medium = '../resources/images/medium.jpg'; */
 
 let inputIds = [];
 
 for (const input of getInput) {
-    inputIds.push(input.id);
-};
+	inputIds.push(input.id);
+}
 
+
+/*
 const images = [wall, white, colors, medium];
 
 const imageLoop = () => {
-    for (let i = 0; i < images.length; i++) {
-        switch (i) {
-            case 0:
-                setTimeout(() => {
-                    body.style.backgroundImage = `url('${images[i]}')`;
-                }, 9000)
-                break;
-            case 1:
-                setTimeout(() => {
-                    body.style.backgroundImage = `url('${images[i]}')`;
-                }, 18000)
-                break;
-            case 2:
-                setTimeout(() => {
-                    body.style.backgroundImage = `url('${images[i]}')`;
-                }, 27000)
-                break;
-            case 3:
-                setTimeout(() => {
-                    body.style.backgroundImage = `url('${images[i]}')`;
-                }, 36000)
-                break;
-            default:
-                setTimeout(() => {
-                    console.log('Done');
-                }, 48000)
-                break;
-        }
+	for (let i = 0; i < images.length; i++) {
+		switch (i) {
+		case 0:
+			setTimeout(() => {
+				body.style.backgroundImage = `url('${images[i]}')`;
+			}, 9000);
+			break;
+		case 1:
+			setTimeout(() => {
+				body.style.backgroundImage = `url('${images[i]}')`;
+			}, 18000);
+			break;
+		case 2:
+			setTimeout(() => {
+				body.style.backgroundImage = `url('${images[i]}')`;
+			}, 27000);
+			break;
+		case 3:
+			setTimeout(() => {
+				body.style.backgroundImage = `url('${images[i]}')`;
+			}, 36000);
+			break;
+		default:
+			setTimeout(() => {
+				console.log('Done');
+			}, 48000);
+			break;
+		}
 
-    }
-}
+	}
+}; */
 
-for (let i = 0; i < 10; i++) {
-    imageLoop();
-}
+const hi = 'Hey';
+
+console.log(hi);					
 
 //console.log(inputIds);
